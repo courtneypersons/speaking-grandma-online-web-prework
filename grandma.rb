fae923e9c2003e4d3e1d20e6f90be2542765ed45
courtney persons <personscourtney@gmail.com>
2018-09-19 01:50:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(speak)
+def speak_to_grandma(speak = "I LOVE YOU TOO PUMPKIN!" )
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
   else
