6ecfe175da65d0286f85b81d7de7d90b96e5c4e8
courtney persons <personscourtney@gmail.com>
2018-09-19 02:23:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 
   #   puts "HUH?! SPEAK UP, SONNY!'"
 
-  def speak_to_grandma(speak, "I LOVE YOU TOO PUMPKIN!")
+  def speak_to_grandma(speak,)
     if speak == speak.upcase
       "NO, NOT SINCE 1938!"
     else
